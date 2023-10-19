06464695624c9b1d6e283514f438bb03cd7bb199
Tushar Mathur <tusharmath@gmail.com>
2023-10-19 20:09:09 +0530

    update generation scripts

--- npm/scripts/generate.js
+++ npm/scripts/generate.js
@@ -2,16 +2,22 @@ import * as fs from "fs/promises"
 
 import * as path from "path"
 import {fileURLToPath} from "url"
+import * as toml from "toml"
 
 const FILE_NAME = "rust-npm-demo"
 
 async function main() {
   const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+  const cargo = await fs
+    .readFile(path.resolve(__dirname, "../../cargo.toml"), "utf-8")
+    .then((data) => JSON.parse(JSON.stringify(toml.parse(data), null, 2)))
+
   const packageJson = await fs
     .readFile(path.resolve(__dirname, "../package.json"), "utf-8")
-
     .then(JSON.parse)
 
+  console.dir(cargo)
   for (const workspace of packageJson.workspaces) {
     const source = `../target/release/${FILE_NAME}`
     const destination = path.resolve(__dirname, `../${workspace}/bin`)
@@ -24,12 +30,13 @@ async function main() {
       path.resolve(source),
       path.resolve(destination, FILE_NAME)
     )
+
     const workspacePackageJson = {
-      name: packageJson.name,
-      version: packageJson.version,
-      description: packageJson.description,
-      author: packageJson.description,
-      license: packageJson.license,
+      name: cargo.package.name,
+      version: cargo.package.version,
+      description: cargo.package.description,
+      author: cargo.package.authors.join(", "),
+      license: cargo.package.license,
       directories: {bin: "bin"},
     }
 
